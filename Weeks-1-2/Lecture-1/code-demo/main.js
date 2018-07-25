4999ebc7da2cbffcdc9d73a9ed4c969508f37a75
NAME <EMAIL>
2018-07-25 18:57:54 -0400

    making a change

--- Weeks-1-2/Lecture-1/code-demo/main.js
+++ Weeks-1-2/Lecture-1/code-demo/main.js
@@ -1,4 +1,15 @@
 let myName = prompt('What\'s your name?');
+let myAge = confirm('Are you older than 30');
 
 console.log(myName);
+console.log(myAge);
+
+if(myAge) {
+    console.log('Hello ' + myName + ' I\'m older than 30 too');
+} else {
+    console.log('Hello ' + myName + ' you\'re so young!');
+}
+
+alert('Hello World');
+
 
