ed082ffb7a5300d68dbfd8ff7419da3101a4b22a
NAME <EMAIL>
2018-07-26 20:04:22 -0400

    pushing code demo for functions and document. methods

--- Weeks-1-2/Lecture-1/code-demo/main.js
+++ Weeks-1-2/Lecture-1/code-demo/main.js
@@ -1,8 +1,10 @@
 let myName = prompt('What\'s your name?');
 let myAge = confirm('Are you older than 30');
+let myAlert = alert('this is an alert');
 
 console.log(myName);
 console.log(myAge);
+console.log(myAlert);
 
 if(myAge) {
     console.log('Hello ' + myName + ' I\'m older than 30 too');
@@ -12,4 +14,24 @@ if(myAge) {
 
 alert('Hello World');
 
+let elName = document.getElementById('name');
+
+elName.innerHTML = myName;
+console.log(elName);
+
+
+
+
+// let myFunc = function(num) {
+//     console.log(num + 2);
+// };
+
+// let callFunc = myFunc(10);
+// console.log(callFunc);
+
+// let newFunc = function(){
+//     console.log(callFunc + 2);
+// };
+
+// newFunc();
 
