916a619fde69109d3ada942b8e02fb2b117073ac
NAME <EMAIL>
2018-08-07 18:18:50 -0400

    proving this works

--- code-exercises/basicFunctions/consoleFunc/consoleFunc.js
+++ code-exercises/basicFunctions/consoleFunc/consoleFunc.js
@@ -1,6 +1,6 @@
 //write a function called consoleFunc that takes in a string and prints that string to the console.
 
-//YOUR FUNCTION HERE
+function consoleFunc
 
 //TEST - to test your function copy the command below into your terminal
 // jest consoleFunc/test.js
