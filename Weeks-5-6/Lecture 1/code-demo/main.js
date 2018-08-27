0054fdaabc730952a404061df69c9a153dab9a48
NAME <EMAIL>
2018-08-27 19:58:03 -0400

    added comments to code demo

--- Weeks-5-6/Lecture 1/code-demo/main.js
+++ Weeks-5-6/Lecture 1/code-demo/main.js
@@ -26,13 +26,19 @@ let DrStrange = new HeroImage('Dr Strange', 'Marvel', './assets/drstrange.jpg');
 //push our new instances/objects to our imageArray
 heroImageArray.push(Thor, Thanos, BlackPanther, Hulk, DeadPool, DrStrange);
 
+//define a function that will select a random image object from our heroImageArray
 function randomImage() {
+    //declare a variable that will calculate a random whole number between 0 and the length of our image array
     let randomNumber = Math.floor(Math.random() * heroImageArray.length);
+    //declare a variable that will store the image object at the index of our random number
     let imageIndex = heroImageArray[randomNumber];
+    //increment the shown property of our random image object by 1
     imageIndex.shown += 1;
+    //return our random image object
     return imageIndex;
 }
 
+//declaring variables that will store the return value for our random image function
 let firstImage = randomImage();
 let secondImage = randomImage();
 let thirdImage = randomImage();
@@ -42,11 +48,13 @@ imageOne.src = firstImage.filePath;
 imageTwo.src = secondImage.filePath;
 imageThree.src = thirdImage.filePath;
 
+//define event handler function that will increment the times clicked for the firstImage
 function  imageClicked() {
     firstImage.clicked += 1;
     console.log('first image clicked property', firstImage.clicked);
 }
 
+//attach an event listener to our imageOne html tag and listen for a click event that will invoke our event handler function
 imageOne.addEventListener('click', imageClicked);
 
 console.log('first image shown', firstImage.shown);
